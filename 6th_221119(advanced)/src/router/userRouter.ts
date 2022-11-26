3e3967611252622507d2f385d3b9f5702d773288
YuSuhwa-ve <dhfhfkzjxms@sookmyung.ac.kr>
2022-11-26 19:26:26 +0900

    [6주차] 도전과제 : 오타수정완료

--- 6th_221119(advanced)/src/router/userRouter.ts
+++ 6th_221119(advanced)/src/router/userRouter.ts
@@ -7,15 +7,15 @@ const userRouter : Router = Router();
 //유저 생성 : 회원가입
 userRouter.post(
     "/signup",
-    /*
+    
     [
         body("name").notEmpty(),
         body("email").notEmpty(),
         body("email").isEmail(),
-        body("age").notEmpty,
-        body("age").isNumeric,
+        body("age").notEmpty(),
+        body("age").isNumeric(),
         body("password").notEmpty(),
-    ],*/()=>{console.log("회원가입 라우터에 들어옴?");},
+    ],
     userController.createUser
     );
 
@@ -24,13 +24,13 @@ userRouter.post(
 //유저 로그인
 userRouter.post(
     "/login",
-/*[
+[
     body("email").notEmpty(),
-    body("email").isEmail,
+    body("email").isEmail(),
     body("password").notEmpty()
 ]
-,*/ ()=>{console.log("로그인 라우터에 들어옴?");}
-,userController.logInUser
+, 
+userController.logInUser
 );
 
 
